640120820b43b3ef304de2fda8e33e036bb8be4d
Sergey Scherbo <sergey31scherbo@gmail.com>
2018-02-11 14:18:36 +0400

    daily page

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,28 @@ $(document).ready(function(){
   var _window = $(window);
   var _document = $(document);
 
+  $.fn.datepicker.language['ru'] = {
+    days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+    daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
+    daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthsShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+    today: 'Сегодня',
+    clear: 'Очистить',
+    dateFormat: 'dd.mm.yyyy',
+    timeFormat: 'hh:ii',
+    firstDay: 1
+  };
+
   $('.datepicker-here').datepicker({
-    inline: true
-  })
+    inline: true,
+    language: 'ru',
+    nextHtml: `<svg class='ico ico-ar'>
+                <use xlink:href='img/sprite.svg#ico-ar'></use>
+              </svg>`,
+    prevHtml: `<svg class='ico ico-arl'>
+                <use xlink:href='img/sprite.svg#ico-arl'></use>
+              </svg>`
+  });
 
 });
