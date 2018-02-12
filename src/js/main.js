da76b6c9d052c3fe83a83dab33111004d1f095f0
Sergey Khmelevskoy <xs290@me.com>
2018-02-12 15:33:38 +0300

    some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,22 @@ $(document).ready(function(){
   var _window = $(window);
   var _document = $(document);
 
+  // svg support for laggy browsers
+  svg4everybody();
+
+  // Viewport units buggyfill
+  window.viewportUnitsBuggyfill.init({
+    force: true,
+    refreshDebounceWait: 150,
+    appendToBody: true
+  });
+
+  // Prevent # behavior
+	_document.on('click', '[href="#"]', function(e) {
+		e.preventDefault();
+	});
+
+  // DATEPICKER
   $.fn.datepicker.language['ru'] = {
     days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
     daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
@@ -23,17 +39,35 @@ $(document).ready(function(){
   $('.datepicker-here').datepicker({
     inline: true,
     language: 'ru',
-    nextHtml: `<svg class='ico ico-ar'>
-                <use xlink:href='img/sprite.svg#ico-ar'></use>
-              </svg>`,
-    prevHtml: `<svg class='ico ico-arl'>
-                <use xlink:href='img/sprite.svg#ico-arl'></use>
-              </svg>`
+    nextHtml: "<svg class='ico ico-ar'><use xlink:href='img/sprite.svg#ico-ar'></use></svg>",
+    prevHtml: "<svg class='ico ico-arl'><use xlink:href='img/sprite.svg#ico-arl'></use></svg>"
   });
 
-  $('a.btn').magnificPopup({
+  // MODALS
+  $('[js-modal]').magnificPopup({
+    type: 'inline',
+    fixedContentPos: true,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
     removalDelay: 300,
     mainClass: 'mfp-fade'
   })
+
+  function closeMfp(){
+    $.magnificPopup.close();
+  }
+
+  _document.on('click', '[js-closeMfp]', function(){
+    closeMfp();
+  })
+
+
+  // MASKED INPUT
+  // $("[js-dateMask]").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
+  // $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
 
 });
